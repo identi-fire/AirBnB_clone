018e65d76b5c78b2df5f610ba02b3b3137dd3ca9
Osigelialex <Osigelialex@gmail.com>
2023-02-10 15:46:24 +0100

    updated file storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,36 +13,44 @@ class FileStorage:
     file storage class serializes instance to a JSON file
     and deserializes JSON file to instances
     """
-    __file_path = ""
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """
         returns the dictionary __objects
         """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """
         sets in __objects the obj with key <obj class name>.id
         """
-        key = eval(type(obj).__name__ + ".id")
-        self.__objects[key] = obj
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            FileStorage.__objects[key] = obj
 
     def save(self):
         """
         serialize __objects to JSON file path
         """
-        with open(self.__file_path, "w") as file:
-            json.dumps(self.__objects, file, indent=4)
+        # modify self.__objects
+        json_objects = {}
+        with open(FileStorage.__file_path, mode="w+", encoding="utf-8") as f:
+            for key, value in FileStorage.__objects.items():
+                json_objects[key] = value.to_dict()
+            json.dump(json_objects, f)
+
 
     def reload(self):
         """
         deserialize the JSON file to __objects
         """
         try:
-            with open(self.__file_path, "r") as file:
-                data = json.load(file)
-                self.__objects.update(data)
-        except FileNotFoundError:
+            with open(FileStorage.__file_path, 'r') as f:
+                dict = json.loads(f.read())
+                for value in dict.values():
+                    cls = value["__class__"]
+                    self.new(eval(cls)(**value))
+        except:
             pass
